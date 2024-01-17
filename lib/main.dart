fbf747d8dec6474d11768e17cd9eadf95be6bf66
Abel Endeshaw <contact.abelend@gmail.com>
2024-01-17 11:23:19 +0300

    added a separater for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ecommerce/utils/theme/theme.dart';
+import 'package:flutter_ecommerce/app.dart';
+
 
 void main() {
   runApp(const App());
-}
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
 }
